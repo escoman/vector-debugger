22cfec16b468f573b289374cb31a2d33423ef88b
svofski <svofski@gmail.com>
2025-07-07 03:20:31 +0400

    runner script updated

--- platform/libretro/run-r36s-aarch64.sh
+++ platform/libretro/run-r36s-aarch64.sh
@@ -1,1 +1,1 @@
-/opt/retroarch/bin/retroarch --config /dev/null -L v06x_libretro.so /roms2/vector06c/incurzion.rom -set "rewind_enable=false" --set "savestate_auto_load=false" --set "savestate_auto_save=false"
+/opt/retroarch/bin/retroarch --config /dev/null -L v06x_libretro.so $1 -set "rewind_enable=false" --set "savestate_auto_load=false" --set "savestate_auto_save=false"
