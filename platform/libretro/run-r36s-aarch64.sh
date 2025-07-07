dc2d1417a8ac9e9959c6d2a368865da4e930e8d1
svofski <svofski@gmail.com>
2025-07-07 04:29:47 +0400

    confirmed, small serialization size makes it run fast on r36s
    regardless of architecture

--- platform/libretro/run-r36s-aarch64.sh
+++ platform/libretro/run-r36s-aarch64.sh
@@ -1,1 +1,2 @@
+# also check if preemptive_frames_enable = "true"
 /opt/retroarch/bin/retroarch --config /dev/null -L v06x_libretro.so $1 -set "rewind_enable=false" --set "savestate_auto_load=false" --set "savestate_auto_save=false"
